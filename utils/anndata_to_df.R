5c5af3fb7cc2db6eb4fc82ce29b1ce10301ff7be
Luqui12 <luca.leomazzi@gmail.com>
2026-02-09 15:07:32 +0100

    added function to load anndata intermediate objects as a data.frame() with metadata annotation for ggplot plotting

--- utils/anndata_to_df.R
+++ utils/anndata_to_df.R
@@ -1,6 +1,19 @@
 source("helper_functions.R")
+
+#' This function is used to convert an (integrated censored) anndata object
+#' into a data.frame() suitable for ggplot plotting.
+#' Metadata information is fetched from the full unintegrated dataset
+#'
+#' @param i_adata AnnData object, integrated data
+#' @param u_adata AnnData object, unintegrated dataset
+#' @param split_id numeric, split id of the integrated data
+#' @return A data.frame() object. The last 7 columns are metadata information, the rest are marker expression.
 anndata_to_df <- function(i_adata, u_adata, split_id){
-  meta <- c("cell_type","batch","sample","donor","group","is_control","split")
   
   out_adata <- get_obs_var_for_integrated(i_adata, u_adata, split_id)
+  out_adata <- subset_markers_tocorrect(out_adata)
+  
+  out_df <- data.frame(out_adata$layers['integrated'])
+  colnames(out_df) <- paste0(out_adata$var[['marker']]," (",out_adata$var[['channel']],")")
+  out_df <- cbind(out_df,out_adata$obs)
 }
