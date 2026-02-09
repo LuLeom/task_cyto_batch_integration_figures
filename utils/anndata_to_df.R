dd81ac4b1a1108ed0bad766930bcbe4dd8efdd5b
Luqui12 <luca.leomazzi@gmail.com>
2026-02-09 14:18:58 +0100

    added GITIGNORE + utils folder

--- utils/anndata_to_df.R
+++ utils/anndata_to_df.R
@@ -1,3 +1,6 @@
-anndata_to_df <- function(adata,meta_to_keep){
+source("helper_functions.R")
+anndata_to_df <- function(i_adata, u_adata, split_id){
+  meta <- c("cell_type","batch","sample","donor","group","is_control","split")
   
+  out_adata <- get_obs_var_for_integrated(i_adata, u_adata, split_id)
 }
